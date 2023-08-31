b4c1a9ffc36a3a148699622a0e5acce759d0e029
ynncstslv <ynncstslv@gmail.com>
2023-08-31 20:06:45 -0300

    feat: mobile sidebar component

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { FC } from 'react';
+import { Button } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Sidebar from './Sidebar';
+
+interface MobileSidebarProps {}
+
+const MobileSidebar: FC<MobileSidebarProps> = ({}) => {
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<Button variant="ghost" size="icon" className="md:hidden">
+					<Menu />
+				</Button>
+			</SheetTrigger>
+			<SheetContent side="left" className="p-0">
+				<Sidebar />
+			</SheetContent>
+		</Sheet>
+	);
+};
+
+export default MobileSidebar;
