1a8a9630448ca0976f87a4d7563af54ab2c63b13
ynncstslv <ynncstslv@gmail.com>
2023-09-05 15:19:52 -0300

    feat: api limit counter component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,14 @@
+import { getApiLimitCount } from '@/lib/apiLimit';
+
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+	const apiLimitCount = await getApiLimitCount();
 	return (
 		<div className="h-full relative">
 			<div className="h-full hidden bg-gray-900 z-[80] md:w-64 md:flex md:flex-col md:fixed md:inset-y-0">
-				<Sidebar />
+				<Sidebar apiLimitCount={apiLimitCount} />
 			</div>
 			<main className="md:pl-64">
 				<Navbar />
