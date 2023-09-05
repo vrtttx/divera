c3a27f3c43266a987eea568fa58b3f90b0750595
ynncstslv <ynncstslv@gmail.com>
2023-09-05 11:12:17 -0300

    fix: /video api route update with api limit

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,3 +1,5 @@
+import { increaseApiLimit, checkApiLimit } from '@/lib/apiLimit';
+
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 import Replicate from 'replicate';
@@ -17,6 +19,11 @@ export async function POST(req: Request) {
 
 		if (!prompt) return new NextResponse('Prompt is required', { status: 400 });
 
+		const freeTrial = await checkApiLimit();
+
+		if (!freeTrial)
+			return new NextResponse('Free trial has expired', { status: 403 });
+
 		const response = await replicate.run(
 			'anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351',
 			{
@@ -25,6 +32,8 @@ export async function POST(req: Request) {
 				},
 			}
 		);
+
+		await increaseApiLimit();
 
 		return NextResponse.json(response);
 	} catch (error) {
